d6df9ba196d5636b86612a94779c24195ccebc2a
Henri Devigne <henri.devigne@bonkgaming.fr>
2020-03-08 17:30:35 +0100

    feat: ignore onbuild image

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ versions=$(wget -q https://registry.hub.docker.com/v1/repositories/golang/tags -
  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' \
  | tr '}' '\n' \
  | awk -F: '{print $3}' \
- | grep -v nano | grep -v windows | grep -v cross)
+ | grep -v nano | grep -v windows | grep -v cross | grep -v onbuild)
 
 for version in ${versions}
 do
