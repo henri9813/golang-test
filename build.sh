f9dd3890a06121412234ec4c4ad0bdbe11cbb179
Henri Devigne <henri@devigne.digital>
2023-08-20 15:22:36 +0200

    feat: add arm platform support

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ if [[ -d logs ]]; then rm -rf logs; fi && mkdir "logs"
 versions=$(cat "versions.txt")
 
 function build() {
-	docker build -t "henri9813/golang-test:${version}" --build-arg "VERSION=${version}" . >>logs/${version}.log
-	docker push "henri9813/golang-test:${version}" >>logs/${version}.log
+  docker buildx create --use --name golang-test-buildx-instance >> logs/${version}.log 2>&1
+	docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v8 -t "henri9813/golang-test:${version}" --build-arg "VERSION=${version}" --push . >> logs/${version}.log 2>&1
 	docker system prune --all --force >>logs/${version}.log
+	docker buildx rm golang-test-buildx-instance >> logs/${version}.log 2>&1
 }
 
 i=1
